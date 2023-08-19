a26cbef2f6d637fbf4e3284c56d00e8886991d75
Ardhi Lukianto <ardhi@lukianto.com>
2023-08-19 18:23:40 +0700

    New helper

---
+++ bajo/helper/build-query.js
@@ -0,0 +1,13 @@
+import nql from '@tryghost/nql'
+
+async function buildQuery (input) {
+  const { importPkg } = this.bajo.helper
+  const { trim, isString, isEmpty, isPlainObject } = await importPkg('lodash-es')
+  if (isEmpty(input)) return
+  if (isPlainObject(input)) return input
+  if (!isString(input)) return
+  if (trim(input).startsWith('{')) return JSON.parse(input)
+  return nql(input)
+}
+
+export default buildQuery
