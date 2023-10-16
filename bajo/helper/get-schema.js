18e4464695fa3ed90a46bbb0103fb5a8c5bba109
Ardhi Lukianto <ardhi@lukianto.com>
2023-10-16 20:41:56 +0700

    Bug fix

--- bajo/helper/get-schema.js
+++ bajo/helper/get-schema.js
@@ -1,11 +1,11 @@
 async function getSchema (name) {
   const { importPkg, error, pascalCase } = this.bajo.helper
-  const { find, isPlainObject } = await importPkg('lodash-es')
+  const { find, isPlainObject, cloneDeep } = await importPkg('lodash-es')
   if (isPlainObject(name)) name = name.name
   name = pascalCase(name)
   const schema = find(this.bajoDb.schemas, { name })
   if (!schema) throw error('Unknown coll/schema \'%s\'', name)
-  return schema
+  return cloneDeep(schema)
 }
 
 export default getSchema
