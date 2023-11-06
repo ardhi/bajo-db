e0f2f3f6411f42178ec2d8c09f7ed04f1d455e50
Ardhi Lukianto <ardhi@lukianto.com>
2023-11-07 05:49:29 +0700

    Updates

--- bajo/helper/record/find.js
+++ bajo/helper/record/find.js
@@ -1,5 +1,23 @@
 import buildRecordAction from '../../../lib/build-record-action.js'
 
+async function handleBboxQuery (filter, schema) {
+  const { importPkg } = this.bajo.helper
+  const { merge } = await importPkg('lodash-es')
+  const props = schema.properties.map(item => item.name)
+  if (filter.bbox && props.includes('lat') && props.includes('lng')) {
+    const [minx, miny, maxx, maxy] = filter.bbox
+    const lng = { $gte: minx, $lte: maxx }
+    const lat = { $gte: miny, $lte: maxy }
+    if (filter.query) {
+      const $or = filter.query.$or
+      if ($or) {
+        filter.query = { lat, lng, $or }
+      } else merge(filter.query, { lng, lat })
+    } else filter.query = { lng, lat }
+    delete filter.bbox
+  }
+}
+
 async function find (name, filter = {}, options = {}) {
   const { runHook } = this.bajo.helper
   const { pickRecord, collExists } = this.bajoDb.helper
@@ -10,6 +28,7 @@ async function find (name, filter = {}, options = {}) {
     await runHook('bajoDb:onBeforeRecordFind', name, filter, options)
     await runHook(`bajoDb.${name}:onBeforeRecordFind`, filter, options)
   }
+  await handleBboxQuery.call(this, filter, schema)
   const records = await handler.call(this, { schema, filter, options })
   if (!skipHook) {
     await runHook(`bajoDb.${name}:onAfterRecordFind`, filter, options, records)
