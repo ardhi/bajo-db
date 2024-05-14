3d0af7018df423cfd2c7843869e76a2b82c143cc
Ardhi Lukianto <ardhi@lukianto.com>
2024-05-14 20:40:25 +0800

    Updates

--- bajo/helper/attachment/copy-uploaded.js
+++ bajo/helper/attachment/copy-uploaded.js
@@ -1,26 +1,27 @@
 import path from 'path'
 
 async function copyUploaded (name, id, { req, setField, setFile, mimeType, stats } = {}) {
-  const { fastGlob, fs, getPluginDataDir } = this.bajo.helper
+  const { fs } = this.bajo.helper
   const { attachmentPreCheck, attachmentCreate } = this.bajoDb.helper
   name = attachmentPreCheck(name)
   if (!name) return
-  const sourceDir = `${getPluginDataDir('bajoWeb')}/upload/${req.id}`
-  const files = await fastGlob(`${sourceDir}/*`)
+  if (!this.bajoWeb) return
+  const { getUploadedFiles } = this.bajoWeb.helper
+  const { dir, files } = await getUploadedFiles(req.id, false, true)
   const result = []
   if (files.length === 0) return result
   for (const f of files) {
-    let [field, ...parts] = path.basename(f).split('-')
+    let [field, ...parts] = path.basename(f).split('@')
     if (parts.length === 0) continue
     field = setField ?? field
-    const file = setFile ?? parts.join('-')
+    const file = setFile ?? parts.join('@')
     const opts = { source: f, field, file, mimeType, stats, req }
     const rec = await attachmentCreate(name, id, opts)
     delete rec.dir
     result.push(rec)
     if (setField || setFile) break
   }
-  await fs.remove(sourceDir)
+  fs.removeSync(dir)
   return result
 }
 
