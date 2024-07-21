98d6b7941b3353890470a75fa1632d0aac75cc73
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-21 07:24:19 +0700

    Bug fix: ns is missing

--- bajo/hook/bajoI18N.db@before-resource-merge.js
+++ bajo/hook/bajoI18N.db@before-resource-merge.js
@@ -1,8 +1,8 @@
 async function beforeResourceMerge (lng, content) {
   const { eachPlugins, readConfig } = this.app.bajo
   const { merge } = this.app.bajo.lib._
-  await eachPlugins(async function ({ file }) {
-    const item = await readConfig(file)
+  await eachPlugins(async function ({ file, ns }) {
+    const item = await readConfig(file, { ns })
     merge(content, item)
   }, { glob: `i18n/${lng}.json`, ns: 'bajoDb' })
 }
